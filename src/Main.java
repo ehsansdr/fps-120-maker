4d490d9edfc207e5bb472c50f73737062c69712c
ehsansdr <sehsan251@gmail.com>
2024-01-02 14:08:15 +0330

    first committ

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         //in this project we have special part that create thread and
         //help us to have 120fps for our graphic project
+        //and I want to store the process to my github to have specific
+        //and optimize algorithm
+
     }
 }
