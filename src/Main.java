7df46017933f41d5b4aca781eba5edbe38aac4d0
ehsansdr <sehsan251@gmail.com>
2024-01-02 14:05:07 +0330

    first commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        //in this project we have special part that create thread and
+        //help us to have 120fps for our graphic project
+    }
+}
